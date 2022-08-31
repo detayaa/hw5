bdadcdc065119246ca0ab22980f45ad9572d1fdb
detayaa <drugochenko.taya@mail.ru>
2022-08-31 18:17:18 +0700

    2

--- src/hw.java
+++ src/hw.java
@@ -8,5 +8,17 @@ public class hw {
         } else {
             System.out.println("Установите версию приложения для iOS по ссылке");
         }
+
+        // задание 2
+        int clientDeviceYear = 2011;
+        if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear >=2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
     }
 }
