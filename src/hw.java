6aa2fba7c515616787dcd9d1727042784f60c1a9
detayaa <drugochenko.taya@mail.ru>
2022-08-31 17:49:05 +0700

    1

--- src/hw.java
+++ src/hw.java
@@ -1,5 +1,12 @@
 public class hw {
     public static void main(String[] args) {
 
+        // задание 1
+        int clientOS = 0;
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
     }
 }
