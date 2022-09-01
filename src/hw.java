d573d270db24a05ee268cec1b78f71035fc1041b
detayaa <drugochenko.taya@mail.ru>
2022-09-01 22:09:38 +0700

    5

--- src/hw.java
+++ src/hw.java
@@ -10,7 +10,7 @@ public class hw {
         }
 
         // задание 2
-        int clientDeviceYear = 2011;
+        int clientDeviceYear = 2016;
         if (clientOS == 1 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         } else if (clientOS == 1 && clientDeviceYear >=2015) {
@@ -20,5 +20,60 @@ public class hw {
         } else if (clientOS == 0 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         }
+
+        // задание 3
+        int year = 2021;
+
+
+
+
+        // задание 4
+        int deliveryDistance = 95;
+
+
+
+        // задание 5
+        int numberMonth = 12;
+        switch (numberMonth) {
+            case 1:
+                System.out.println("Январь");
+                break;
+            case 2:
+                System.out.println("Февраль");
+                break;
+            case 3:
+                System.out.println("Март");
+                break;
+            case 4:
+                System.out.println("Апрель");
+                break;
+            case 5:
+                System.out.println("Май");
+                break;
+            case 6:
+                System.out.println("Июнь");
+                break;
+            case 7:
+                System.out.println("Июль");
+                break;
+            case 8:
+                System.out.println("Август");
+                break;
+            case 9:
+                System.out.println("Сентябрь");
+                break;
+            case 10:
+                System.out.println("Октябрь");
+                break;
+            case 11:
+                System.out.println("Ноябрь");
+                break;
+            case 12:
+                System.out.println("Декабрь");
+                break;
+        }
+
+
     }
 }
+
